b0780a6465002fb8d7fe8ad793bc9c04663f2414
Sergei Pradko <rodzy28@yandex.ru>
2023-01-06 15:25:55 +0300

    Добавить в popup элементы

--- popup.js
+++ popup.js
@@ -1,10 +1,11 @@
 let popup = document.querySelector('.profile__edit-button');
+
 let popupShow = document.querySelector('.popup');
 let popupUnShow = document.querySelector('.popup__close-button');
 
-function openPopup () {
+function editPopup () {
   popupShow.classList.toggle('popup_opened');
 }
 
-popup.addEventListener('click', openPopup);
-popupUnShow.addEventListener('click', openPopup);
+popup.addEventListener('click', editPopup);
+popupUnShow.addEventListener('click', editPopup);
