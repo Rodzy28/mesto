24087829cda50c48b1324b69aec051dc97f9a38b
Sergei Pradko <rodzy28@yandex.ru>
2023-01-22 00:01:18 +0300

    Исправить все замечания ревьювера

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,21 +1,20 @@
+let popupProfile = document.querySelector('.popup');
 let btnEdit = document.querySelector('.profile__edit-button');
 let btnClose = document.querySelector('.popup__close-button');
 let profileName = document.querySelector('.profile__name');
 let profileJob = document.querySelector('.profile__job');
-
-
 let formElement = document.querySelector('.popup__container');
-let nameInput = document.querySelector('.popup__input_name');
-let jobInput = document.querySelector('.popup__input_job');
+let nameInput = document.querySelector('.popup__input_type_name');
+let jobInput = document.querySelector('.popup__input_type_job');
 
 function popupShow() {
-  document.querySelector('.popup').classList.add('popup_opened');
+  popupProfile.classList.add('popup_opened');
   nameInput.value = profileName.textContent;
   jobInput.value = profileJob.textContent;
 }
 
 function popupClose() {
-  document.querySelector('.popup').classList.remove('popup_opened');
+  popupProfile.classList.remove('popup_opened');
 }
 
 btnEdit.addEventListener('click', popupShow);
