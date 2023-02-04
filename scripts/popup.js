d254dbc746cbada0c4ecf56ea67dbc052dd5ef56
Sergei Pradko <rodzy28@yandex.ru>
2023-02-04 18:59:03 +0300

    Доделать попап просмотра фото

--- scripts/popup.js
+++ scripts/popup.js
@@ -4,8 +4,11 @@ const cardPopup = document.querySelector('.popup_type_card');
 // Инпуты попап
 const nameInput = document.querySelector('.popup__input_type_name');
 const jobInput = document.querySelector('.popup__input_type_job');
+const placeInput = document.querySelector('.popup__input_type_place');
+const srcInput = document.querySelector('.popup__input_type_src');
 // Формы попап
 const formElement = document.querySelector('.popup__form-profile');
+const formElementCard = document.querySelector('.popup__form-card');
 // Кнопки попап
 const btnEdit = document.querySelector('.profile__edit-button');
 const btnClose = document.querySelectorAll('.popup__close-button');
@@ -13,6 +16,9 @@ const btnAddCard = document.querySelector('.profile__add-button');
 // Получение активных данных профиля
 const profileName = document.querySelector('.profile__name');
 const profileJob = document.querySelector('.profile__job');
+// Шаблон карточек
+const listCards = document.querySelector('.place__list');
+const templateCard = document.querySelector('.place__card').content.querySelector('.place__item');
 // Первые 6 карточек
 const initialCards = [
   { name: 'Шри-Ланка', link: './images/pictures/sri_lanka.jpg' },
@@ -54,29 +60,35 @@ function handleFormSubmit(evt) {
   profileJob.textContent = jobInput.value;
   closePopup(profilePopup);
 }
-
 formElement.addEventListener('submit', handleFormSubmit);
 
-const listCards = document.querySelector('.place__list');
-const templateCard = document.querySelector('.place__card').content.querySelector('.place__item');
-
-function createCard() {
-  initialCards.forEach((item) => {
-    const card = templateCard.cloneNode(true);
-    const btnLike = card.querySelector('.place__like-button');
-    const btnTrash = card.querySelector('.place__trash-button');
-    card.querySelector('.place__text').textContent = item.name;
-    card.querySelector('.place__picture').src = item.link;
-    card.querySelector('.place__picture').alt = item.name;
-    btnLike.addEventListener('click', (evt) => {
-      evt.target.classList.toggle('place__like-button_active');
-    });
-    btnTrash.addEventListener('click', () => {
-      card.remove();
-    });
-    listCards.append(card);
+function cardTemplate(newPlace, newSrc) {
+  const card = templateCard.cloneNode(true);
+  card.querySelector('.place__text').textContent = newPlace;
+  card.querySelector('.place__picture').src = newSrc;
+  card.querySelector('.place__picture').alt = newPlace;
+  const btnLike = card.querySelector('.place__like-button');
+  const btnTrash = card.querySelector('.place__trash-button');
+  btnLike.addEventListener('click', (evt) => {
+    evt.target.classList.toggle('place__like-button_active');
   });
+  btnTrash.addEventListener('click', () => {
+    card.remove();
+  });
+  return card;
 }
 
-createCard();
+function handleFormSubmitCard(evt) {
+  evt.preventDefault();
+  const card = cardTemplate(placeInput.value, srcInput.value);
+  closePopup(cardPopup);
+  listCards.prepend(card);
+}
+formElementCard.addEventListener('submit', handleFormSubmitCard);
 
+function defaultCards() {
+  initialCards.forEach((item) => {
+    listCards.append(cardTemplate(item.name, item.link));
+  });
+}
+defaultCards();
