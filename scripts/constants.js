3c1d6d4cc709327d020c102f844f6a85bc00bd80
Sergei Pradko <rodzy28@yandex.ru>
2023-02-06 21:25:56 +0300

    Исправить замечания ревьювера

---
+++ scripts/constants.js
@@ -0,0 +1,9 @@
+// Первые 6 карточек
+const initialCards = [
+  { name: 'Шри-Ланка', link: './images/pictures/sri_lanka.jpg' },
+  { name: 'Кипр', link: './images/pictures/cyprus.jpg' },
+  { name: 'Белёв', link: './images/pictures/belev.jpg' },
+  { name: 'Собачий приют - Некрасовка', link: './images/pictures/nekrasovka.jpg' },
+  { name: 'Тайланд', link: './images/pictures/thailand.jpg' },
+  { name: 'Дубай', link: './images/pictures/dubai.jpg' }
+];
--- scripts/popup.js
+++ scripts/popup.js
@@ -1,5 +1,5 @@
 // Попап
-const allPopup = document.querySelectorAll('.popup');
+const allPopups = document.querySelectorAll('.popup');
 const profilePopup = document.querySelector('.popup_type_profile');
 const cardPopup = document.querySelector('.popup_type_card');
 const imagePopup = document.querySelector('.popup_type_image');
@@ -20,15 +20,6 @@ const profileJob = document.querySelector('.profile__job');
 // Шаблон карточек
 const listCards = document.querySelector('.place__list');
 const templateCard = document.querySelector('.place__card').content.querySelector('.place__item');
-// Первые 6 карточек
-const initialCards = [
-  { name: 'Шри-Ланка', link: './images/pictures/sri_lanka.jpg' },
-  { name: 'Кипр', link: './images/pictures/cyprus.jpg' },
-  { name: 'Белёв', link: './images/pictures/belev.jpg' },
-  { name: 'Собачий приют - Некрасовка', link: './images/pictures/nekrasovka.jpg' },
-  { name: 'Тайланд', link: './images/pictures/thailand.jpg' },
-  { name: 'Дубай', link: './images/pictures/dubai.jpg' }
-];
 
 function openPopup(popup) {
   popup.classList.add('popup_opened');
@@ -51,29 +42,30 @@ btnAddCard.addEventListener('click', () => {
 });
 
 // Закрывашка попапов
-allPopup.forEach((item) => {
+allPopups.forEach((item) => {
   item.querySelector('.popup__close-button').addEventListener('click', () => {
     closePopup(item);
   });
 });
 
 // Получить новые данные имени и профессии от пользователя
-function handleFormSubmit(evt) {
+function handleFormSubmitProfile(evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileJob.textContent = jobInput.value;
   closePopup(profilePopup);
 }
-formElementProfile.addEventListener('submit', handleFormSubmit);
+formElementProfile.addEventListener('submit', handleFormSubmitProfile);
 
 // Создать карточку нового места
-function cardTemplate(newPlace, newSrc) {
+function createNewCard(newPlace, newSrc) {
   const card = templateCard.cloneNode(true);
-  card.querySelector('.place__text').textContent = newPlace;
-  card.querySelector('.place__picture').src = newSrc;
-  card.querySelector('.place__picture').alt = newPlace;
+  const showCardPicture = card.querySelector('.place__picture');
   const btnLike = card.querySelector('.place__like-button');
   const btnTrash = card.querySelector('.place__trash-button');
+  showCardPicture.src = newSrc;
+  showCardPicture.alt = newPlace;
+  card.querySelector('.place__text').textContent = newPlace;
   btnLike.addEventListener('click', (evt) => {
     evt.target.classList.toggle('place__like-button_active');
   });
@@ -82,8 +74,9 @@ function cardTemplate(newPlace, newSrc) {
   });
   card.querySelector('.place__picture').addEventListener('click', () => {
     openPopup(imagePopup);
-    imagePopup.querySelector('.popup__image-viewing').src = newSrc;
-    imagePopup.querySelector('.popup__image-title').alt = newPlace;
+    const imageViewing = imagePopup.querySelector('.popup__image-viewing');
+    imageViewing.src = newSrc;
+    imageViewing.alt = newPlace;
     imagePopup.querySelector('.popup__image-title').textContent = newPlace;
   })
   return card;
@@ -92,30 +85,30 @@ function cardTemplate(newPlace, newSrc) {
 // Получить новое имя места, ссылку, добавить на сайт карточку
 function handleFormSubmitCard(evt) {
   evt.preventDefault();
-  const card = cardTemplate(placeInput.value, srcInput.value);
+  const card = createNewCard(placeInput.value, srcInput.value);
   closePopup(cardPopup);
   listCards.prepend(card);
 }
 formElementCard.addEventListener('submit', handleFormSubmitCard);
 
 // Показать 6 дефолтных карточек
-function defaultCards() {
+function renderDefaultCards() {
   initialCards.forEach((item) => {
-    listCards.append(cardTemplate(item.name, item.link));
+    listCards.append(createNewCard(item.name, item.link));
   });
 }
-defaultCards();
+renderDefaultCards();
 
-const job = [
+const jobsArray = [
   'Папин бродяга, Мамин симоптяга',
   'Городской сумасшедший',
   'Щ-щ-щегол',
   'Вечно молодой, вечно пьяный'
 ];
 
-function randomJob() {
-  const index = Math.floor(Math.random() * job.length);
-  profileJob.textContent = job[index];
+function fillRandomJob() {
+  const index = Math.floor(Math.random() * jobsArray.length);
+  profileJob.textContent = jobsArray[index];
 }
-randomJob();
+fillRandomJob();
 
